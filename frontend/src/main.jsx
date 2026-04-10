3fc9c5fa27e50dae4036659e086dad7fc15dace8
wueme-byte <wueme-byte@users.noreply.github.com>
2026-04-10 23:36:34 +0300

    revert: remove Telegram WebApp expand

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-if (window.Telegram?.WebApp) {
-  window.Telegram.WebApp.expand()
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
